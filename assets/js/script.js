22995f244d773122b7872e700462dd744ad85c8f
deboradom <deboradom@gmail.com>
2018-05-09 23:35:07 -0300

    dropdown v1

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,13 @@ jQuery('a').click(function(){
     }, 500);
     return false;
 }); 
+
+// Dropdown Menu
+// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_js_dropdown
+
+document.querySelector(".menu").onclick = function() {myFunction()};
+
+/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}

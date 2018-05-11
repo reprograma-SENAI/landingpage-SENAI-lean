1067a9d992de239e799f1bdbfc4e2affc7bb30ba
deboradom <deboradom@gmail.com>
2018-05-11 10:57:31 -0300

    completo-v1

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,12 +6,23 @@ $(document).on("scroll", function() {
 	} else {
 		$("nav").removeClass("small").addClass("large");
 	}
+
+
+	if($(document).scrollTop()>100) {
+		$("div.dropdown").removeClass("dp-largeNav").addClass("dp-smallNav");
+	} else {
+		$("div.dropdown").removeClass("dp-smallNav").addClass("dp-largeNav");
+	}
+
+
+
+
 });
 
 // ScrollTop para os links da barra de navegação
 jQuery('a').click(function(){
     jQuery('html, body').animate({
-        scrollTop: jQuery( jQuery(this).attr('href') ).offset().top
+        scrollTop: jQuery( jQuery(this).attr('href') ).offset().top -110
     }, 500);
     return false;
 }); 
@@ -24,5 +35,9 @@ jQuery('a').click(function(){
 // Mostrar menu dropdown deslizando
 $(".menu").click(function(){
 	$(".dropdown").slideToggle();
+});
+
+$("a.dropdown-link").click(function(){
+    $(".dropdown").hide();
 });
 

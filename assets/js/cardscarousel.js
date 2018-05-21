f96ddc6d0853730a8296ef2d0a020444f38c0ed1
wwwbrunagon <wwwbrunagon@gmail.com>
2018-05-21 00:51:43 -0300

    Carousel Responsive Version

--- assets/js/cardscarousel.js
+++ assets/js/cardscarousel.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
 
 
           {
-            breakpoint: 400,
+            breakpoint: 768,
             settings: {
               arrows: false,
               centerMode: false,
@@ -33,3 +33,34 @@ $(document).ready(function(){
 
 //unslick: true,
 
+//carousel cards
+
+$(document).ready(function(){
+   $('.passos').slick({
+     //  slidesToShow: 3,
+      // slidesToScroll: 1,
+      centerMode: false,
+      dots: false,
+      autoplay: true,
+      autoplaySpeed: 2000,
+       cssEase: 'linear',
+
+              responsive: [
+          {
+            breakpoint: 7000,
+              settings: 'unslick'
+          },
+
+
+          {
+            breakpoint: 501,
+            settings: {
+              arrows: false,
+              centerMode: false,
+              slidesToShow: 1
+            }
+          }
+        ]
+      
+      });
+});
